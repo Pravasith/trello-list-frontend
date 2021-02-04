5d7e8e2c53a8d735c8b3015a566d7d17b7a6fb9d
Pravas Chinthoju <pravasith@gmail.com>
2021-02-04 12:14:11 +0530

    added a basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import { useState } from 'react'
+import DatePicker from 'react-datepicker'
+
+import styles from './styles/app.module.scss'
+import './styles/libs/global.scss'
+
+
+
+const App = () => {
+
+	const [startDate, setStartDate] = useState(new Date())
+
+
+
+
+	return (
+		<div className={`${styles.backgroundWrap}`}>
+			<header className="App-header">
+				<h1>Hello World</h1>
+			</header>
+
+			<div className={`${styles.datePick}`}>
+				<DatePicker selected={startDate} onChange={date => setStartDate(date as Date)}/>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
